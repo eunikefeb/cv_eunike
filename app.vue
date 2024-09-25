3e483814330a7d59a8b60a6dd61267f3585315bf
eunike febianti <eunikefebianti93@gmail.com>
2024-09-25 16:24:42 +0700

    fix try plis 5

--- app.vue
+++ app.vue
@@ -2,28 +2,23 @@
   <div>
     <!-- Hero Section -->
     <section class="bg-dark text-light text-center py-3">
-      <div class="container">
+      <div class="container-fluid">
         <h1 class="display-4">Eunike Febianti</h1>
         <p class="lead">UI/UX Designer | Ngunut, Tulungagung | eunikefebianti93@gmail.com | 087860368942</p>
         <hr class="my-4" />
-        <p class="text-white">Experienced in designing web and mobile applications with a user-centered design approach. Expert in using design tools such as Figma and Canva to produce clear and effective wireframes, prototypes, and user flows.
-          My expertise includes user research, responsive interface design, visual design, and usability testing to ensure the products developed are not only functional but also provide an optimal user experience.
-          I have excellent communication skills and work closely with development teams to transform business needs into high-quality design solutions. Always open to new challenges and has a high commitment to continuing to develop in the field of UI/UX design.</p>
+        <p class="text-white">Experienced in designing web and mobile applications with a user-centered design approach. Expert in using design tools such as Figma and Canva to produce clear and effective wireframes, prototypes, and user flows. My expertise includes user research, responsive interface design, visual design, and usability testing to ensure the products developed are not only functional but also provide an optimal user experience. I have excellent communication skills and work closely with development teams to transform business needs into high-quality design solutions. Always open to new challenges and has a high commitment to continuing to develop in the field of UI/UX design.</p>
       </div>
     </section>
 
     <!-- Education Section -->
     <section class="bg-light py-5">
-      <div class="container">
+      <div class="container-fluid">
         <div class="row align-items-center">
-          <!-- Judul Education -->
           <div class="col-md-2">
             <h2 class="text-left mb-0">Education</h2>
           </div>
-          <!-- Kartu-kartu Education -->
           <div class="col-md-10">
             <div class="row">
-              <!-- SMK Telkom Malang -->
               <div class="col-md-6 d-flex">
                 <div class="card my-3 flex-fill shadow-sm">
                   <div class="card-body">
@@ -39,7 +34,6 @@
                 </div>
               </div>
 
-              <!-- Universitas Brawijaya -->
               <div class="col-md-6 d-flex">
                 <div class="card my-3 flex-fill shadow-sm">
                   <div class="card-body">
@@ -60,39 +54,30 @@
       </div>
     </section>
 
-
     <!-- Work Experience Section -->
     <section class="py-5">
-      <div class="container">
+      <div class="container-fluid">
         <h2 class="text-center">Work Experience</h2>
         <div class="row">
-          <!-- Example Job -->
           <div class="col-md-6">
             <div class="card my-3">
               <div class="card-body">
                 <h5 class="card-title">App Developer</h5>
                 <h6 class="card-subtitle mb-2 text-muted">PT. Thunderlabs Indonesia | Jun 2022 - Nov 2022</h6>
                 <p class="card-text">
-                      Pass the internship according to the specified time limit,
-                      Acts as an active coordinator in providing information to apprentice members,
-                      Complete the project properly and correctly according to the supervisor's directions,
-                      Actively contribute to the work on the Nakoa-Checker project,
-                      Working on the Nakoa-Checker project, which is a Kitchen-App made specifically for the kitchen section
+                  Pass the internship according to the specified time limit, Acts as an active coordinator in providing information to apprentice members, Complete the project properly and correctly according to the supervisor's directions, Actively contribute to the work on the Nakoa-Checker project, Working on the Nakoa-Checker project, which is a Kitchen-App made specifically for the kitchen section.
                 </p>
               </div>
             </div>
           </div>
 
-          <!-- Another Example Job -->
           <div class="col-md-6">
             <div class="card my-3">
               <div class="card-body">
                 <h5 class="card-title">UI/UX Designer</h5>
                 <h6 class="card-subtitle mb-2 text-muted">PT. Thunderlabs Indonesia | Jun 2022 - Nov 2022</h6>
                 <p class="card-text">
-                  Secara aktif berkontribusi dalam pengerjaan proyek Nakoa-Checker, aplikasi dapur yang dirancang khusus untuk bagian dapur,
-                  Bertanggung jawab untuk merancang antarmuka pengguna (UI) dan pengalaman pengguna (UX) pada proyek Nakoa-Checker, mulai dari wireframe, prototipe, hingga desain akhir,
-                  Melakukan penelitian pengguna (user research), uji kegunaan (usability testing), dan berkolaborasi dengan tim pengembang untuk menghasilkan solusi desain yang optimal.
+                  Secara aktif berkontribusi dalam pengerjaan proyek Nakoa-Checker, aplikasi dapur yang dirancang khusus untuk bagian dapur, Bertanggung jawab untuk merancang antarmuka pengguna (UI) dan pengalaman pengguna (UX) pada proyek Nakoa-Checker, mulai dari wireframe, prototipe, hingga desain akhir, Melakukan penelitian pengguna (user research), uji kegunaan (usability testing), dan berkolaborasi dengan tim pengembang untuk menghasilkan solusi desain yang optimal.
                 </p>
               </div>
             </div>
@@ -103,7 +88,7 @@
 
     <!-- Skills Section -->
     <section class="bg-light py-5">
-      <div class="container">
+      <div class="container-fluid">
         <h2 class="text-center">Skills</h2>
         <div class="row text-center">
           <div class="col-md-4">
@@ -127,10 +112,9 @@
 
     <!-- Certification Section -->
     <section class="py-5">
-      <div class="container">
+      <div class="container-fluid">
         <h2 class="text-center">Certification</h2>
         <div class="row d-flex justify-content-center">
-          <!-- Certificate 1 -->
           <div class="col-md-4 d-flex">
             <div class="card my-3 flex-fill">
               <div class="card-body">
@@ -143,7 +127,6 @@
             </div>
           </div>
 
-          <!-- Certificate 2 -->
           <div class="col-md-4 d-flex">
             <div class="card my-3 flex-fill">
               <div class="card-body">
@@ -156,7 +139,6 @@
             </div>
           </div>
 
-          <!-- Certificate 3 -->
           <div class="col-md-4 d-flex">
             <div class="card my-3 flex-fill">
               <div class="card-body">
@@ -172,10 +154,9 @@
       </div>
     </section>
 
-
     <!-- Contact Section -->
     <section class="bg-light py-5">
-      <div class="container">
+      <div class="container-fluid">
         <div class="row align-items-center">
           <div class="col-md-2 text-left">
             <h2>Contact</h2> 
@@ -184,22 +165,22 @@
             <div class="d-flex justify-content-end align-items-center gap-3">
               <div class="mr-4">
                 <a href="mailto:eunikefebianti93@gmail.com" target="_blank">
-                  <img src="/img/Gmail-logo.png" alt="Email Icon" style="width: 40px;">
+                  <img src="/img/Gmail-logo.png" alt="Email Icon" style="width: 40px;" loading="lazy">
                 </a>
               </div>
               <div class="mr-4">
                 <a href="https://www.linkedin.com/in/eunike-febianti-239645225/" target="_blank">
-                  <img src="/img/LinkedIn-logo.png" alt="LinkedIn Icon" style="width: 40px;">
+                  <img src="/img/LinkedIn-logo.png" alt="LinkedIn Icon" style="width: 40px;" loading="lazy">
                 </a>
               </div>
               <div>
                 <a href="https://www.instagram.com/eunikefeb/" target="_blank">
-                  <img src="/img/instagram-logo.png" alt="Instagram Icon" style="width: 40px;">
+                  <img src="/img/instagram-logo.png" alt="Instagram Icon" style="width: 40px;" loading="lazy">
                 </a>
               </div>
               <div>
                 <a href="https://github.com/eunikefeb" target="_blank">
-                  <img src="/img/GitHub-logo.png" alt="GitHub Icon" style="width: 40px;">
+                  <img src="/img/GitHub-logo.png" alt="GitHub Icon" style="width: 40px;" loading="lazy">
                 </a>
               </div>
             </div>
